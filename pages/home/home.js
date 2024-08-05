c82bdcf7081d6dc2ebdd5b1cafea3eb6f5944292
salumanding <saluman.ryan@gmail.com>
2024-08-05 12:21:40 +0800

    feat: update

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,5 +1,9 @@
-import { fetchHome } from '../../services/home/home';
-import { fetchGoodsList } from '../../services/good/fetchGoods';
+import {
+  fetchHome
+} from '../../services/home/home';
+import {
+  fetchGoodsList
+} from '../../services/good/fetchGoods';
 import Toast from 'tdesign-miniprogram/toast/index';
 
 Page({
@@ -13,13 +17,17 @@ Page({
     autoplay: true,
     duration: '500',
     interval: 5000,
-    navigation: { type: 'dots' },
-    swiperImageProps: { mode: 'scaleToFill' },
+    navigation: {
+      type: 'dots'
+    },
+    swiperImageProps: {
+      mode: 'scaleToFill'
+    },
   },
 
   goodListPagination: {
     index: 0,
-    num: 20,
+    num: 4,
   },
 
   privateData: {
@@ -35,9 +43,7 @@ Page({
   },
 
   onReachBottom() {
-    if (this.data.goodsListLoadStatus === 0) {
-      this.loadGoodsList();
-    }
+
   },
 
   onPullDownRefresh() {
@@ -54,7 +60,10 @@ Page({
     this.setData({
       pageLoading: true,
     });
-    fetchHome().then(({ swiper, tabList }) => {
+    fetchHome().then(({
+      swiper,
+      tabList
+    }) => {
       this.setData({
         tabList,
         imgSrcs: swiper,
@@ -80,7 +89,9 @@ Page({
       });
     }
 
-    this.setData({ goodsListLoadStatus: 1 });
+    this.setData({
+      goodsListLoadStatus: 1
+    });
 
     const pageSize = this.goodListPagination.num;
     let pageIndex = this.privateData.tabIndex * pageSize + this.goodListPagination.index + 1;
@@ -98,13 +109,19 @@ Page({
       this.goodListPagination.index = pageIndex;
       this.goodListPagination.num = pageSize;
     } catch (err) {
-      this.setData({ goodsListLoadStatus: 3 });
+      this.setData({
+        goodsListLoadStatus: 3
+      });
     }
   },
 
   goodListClickHandle(e) {
-    const { index } = e.detail;
-    const { spuId } = this.data.goodsList[index];
+    const {
+      index
+    } = e.detail;
+    const {
+      spuId
+    } = this.data.goodsList[index];
     wx.navigateTo({
       url: `/pages/goods/details/index?spuId=${spuId}`,
     });
@@ -119,13 +136,74 @@ Page({
   },
 
   navToSearchPage() {
-    wx.navigateTo({ url: '/pages/goods/search/index' });
+    wx.navigateTo({
+      url: '/pages/goods/search/index'
+    });
   },
 
-  navToActivityDetail({ detail }) {
-    const { index: promotionID = 0 } = detail || {};
+  navToActivityDetail({
+    detail
+  }) {
+    const {
+      index: promotionID = 0
+    } = detail || {};
     wx.navigateTo({
       url: `/pages/promotion-detail/index?promotion_id=${promotionID}`,
     });
   },
+
+  // 跳转咨询页面
+  jump2information(e) {
+    const {
+      currentTarget: {
+        dataset: {
+          id
+        }
+      }
+    } = e
+
+    console.log('id', id)
+
+    if (id) {
+      // 跳转到具体的咨询详情页面
+      wx.navigateTo({
+        url: 'url',
+      })
+    }
+
+    // 跳转到咨询首页
+    wx.navigateTo({
+      url: 'url',
+    })
+  },
+  // 金刚位跳转
+  jump2jingang(e) {
+    const {
+      currentTarget: {
+        dataset: {
+          type
+        }
+      }
+    } = e
+
+    console.log('type', type)
+
+    switch (type) {
+      case '1':
+        // 学习咨询
+        // wx.navigateTo({
+        //   url: 'url',
+        // })
+        break
+      case '2':
+        // 六合简书
+        break
+      case '3':
+        // 直播预告
+        break
+      case '4':
+        // 学习商城
+        break
+    }
+  }
 });
