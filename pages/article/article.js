8337771e58487f38ec56394477c9705972c43833
salumanding <saluman.ryan@gmail.com>
2024-08-18 13:39:59 +0800

    fix: article link

--- pages/article/article.js
+++ pages/article/article.js
@@ -4,12 +4,15 @@ Page({
     url: '' // 初始化为一个空字符串
   },
 
-  onLoad({ article }) {
+  onLoad({
+    article
+  }) {
     // 判断参数是否存在，并设置 URL
     if (article) {
       return this.setData({
-        url: decodeURIComponent(`htps://mp.weixin.qq.com/s/${article}`)
+        url: decodeURIComponent(`https://mp.weixin.qq.com/s/${article}`)
       });
+
     }
 
     // 处理没有提供 article 参数的情况
