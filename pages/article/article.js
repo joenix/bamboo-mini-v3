e65cfbde85a8d08d03277e406064b9ef22c784e6
Joenix <joenix.com@gmail.com>
2024-08-18 12:51:57 +0800

    feat: update

---
+++ pages/article/article.js
@@ -0,0 +1,18 @@
+// article.js
+Page({
+  data: {
+    url: '' // 初始化为一个空字符串
+  },
+
+  onLoad({ article }) {
+    // 判断参数是否存在，并设置 URL
+    if (article) {
+      return this.setData({
+        url: decodeURIComponent(article)
+      });
+    }
+
+    // 处理没有提供 article 参数的情况
+    console.log('No article parameter provided');
+  }
+});
