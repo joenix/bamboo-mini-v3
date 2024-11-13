5f5dabf8859435edfe2b340cf94d13bc21a82964
wenli.li <wenli.li@xbotspace.com>
2024-11-13 11:09:04 +0800

    feat: web-view

--- homePackage/pages/home/lesson/lesson.js
+++ homePackage/pages/home/lesson/lesson.js
@@ -19,13 +19,13 @@ Page({
     const { name: title, content, remark, updatedAt: datetime } = await post(api.Information.detail, { id });
     const lesson = { title, datetime: formatTime(datetime, 'YYYY年MM月DD日 HH:MM'), content, remark };
 
-    lesson.content = lesson.content.split(',');
+    // lesson.content = lesson.content.split(',');
 
-    lesson.remark = lesson.remark
-      // Image
-      .replace(/<img/gi, '<img style="max-width: 100%; margin: .75rem 0 .25rem;"')
-      // Part
-      .replace(/<p/gi, '<p style="text-align: justify; margin: .75rem 0 .25rem; color: #363636;');
+    // lesson.remark = lesson.remark
+    //   // Image
+    //   .replace(/<img/gi, '<img style="max-width: 100%; margin: .75rem 0 .25rem;"')
+    //   // Part
+    //   .replace(/<p/gi, '<p style="text-align: justify; margin: .75rem 0 .25rem; color: #363636;');
 
     this.setData({ lesson });
   }
